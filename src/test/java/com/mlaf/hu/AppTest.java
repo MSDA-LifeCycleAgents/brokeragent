ce1a13f3a2b9cbcf3f6f2d1334a90664d8ef4168
thijsgelton <thijs@shoppingminds.com>
2017-11-06 11:14:59 +0100

    Voor brauke

---
+++ src/test/java/com/mlaf/hu/AppTest.java
@@ -0,0 +1,38 @@
+package com.mlaf.hu;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BrokerAgentTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BrokerAgentTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BrokerAgentTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
