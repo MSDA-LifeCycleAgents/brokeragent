0fcc5a2513efea94be3bae1828e4ec5bdbbefe2d
thijsgelton <thijs.gelton@hotmail.com>
2017-12-19 12:43:39 +0100

    Removing sensoragent feature out of this feature
    This reverts commit f785b0b78f9fc3b83a010e5c6f7a379a48182334.

--- src/main/java/com/mlaf/hu/sensoragent/Sensor.java
+++
@@ -1,9 +0,0 @@
-package com.mlaf.hu.sensoragent;
-
-public interface Sensor {
-    public boolean activate();
-    public boolean deactivate();
-    public String getSensorID();
-    public String getValue();
-    public String getUnit();
-}
